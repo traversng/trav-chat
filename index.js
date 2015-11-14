3f9c880ba147c09cf9e4d7b61bf2d6bdf67ea1c1
Travis Ueki <travis.ueki@learningfuze.com>
2015-11-14 10:37:51 -0800

    added google login/auth, still need to figure out how to push messages to firebase to the users object

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    Firebase = require("firebase");
+    Firebase = require("firebase"),
+    fs = require('fs'),
     users = {};
 
 server.listen(3000);
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/index.html');
+app.get('*', function(req, res){
+    var requestedPage = __dirname + req.path;
+    console.log('req is ',requestedPage);
+    fs.stat(requestedPage, function(err, stat){
+        if(err == null){
+            console.log('file exists ');
+            console.log('res: ',res);
+            console.log('requestedPage: ',requestedPage);
+            res.sendfile(requestedPage);
+        }else if(err.code == 'ENOENT'){
+            res.send("<h1>Error: " + res.statusCode + res.statusMessage + '</h1>');
+        }else{
+            console.log('Some other error: ', err.code);
+        }
+    });
+    //res.sendfile(__dirname + '/index.html');
 });
 
 
@@ -19,6 +34,22 @@ io.sockets.on('connection' , function(socket){
         } else {
             callback(true);
             socket.nickname = name;
+            users[socket.nickname] = socket;
+            //io.sockets.emit('new message' , {nick: socket.nickname, msg: "has connected :)"});
+            updateUsers();
+            console.log('index.js line 16, logged users: ', Object.keys(users).length + " new user: " + name);
+        }
+    });
+
+    // Google authentication event
+    socket.on('successful login', function(name, callback){
+        console.log('serverside successful login name: ', name);
+        var user = name.name;
+        if (user in users){
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = user;
             users[socket.nickname] = socket;
             //io.sockets.emit('new message' , {nick: socket.nickname, msg: "has connected :)"});
             updateUsers();
