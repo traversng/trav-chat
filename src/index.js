5e45bdd0301d6595a52b6f41ad6fe91191008555
Travis Ueki <travis.ueki@learningfuze.com>
2016-05-03 10:02:47 -0700

    index cleanup

--- src/index.js
+++ src/index.js
@@ -8,37 +8,8 @@ import router from './router'
 // ================ ACTIONS ================== //
 import actionNewChatItem from './actions/action-new-chat-text'
 
-
-
-
-// ================ REDUCERS ================= // 
-import chatItemReducer from './reducers/reducer-chat-item'
-import chatListReducer from './reducers/reducer-chat-list'
-
-
-
 // ================== COMBINE REDUCERS ============ //
 import chatApp from './reducers/chatReducer'
-
-// ================== MIDDLEWARE ================= //
-
-// const actionLogger = ({ dispatch, getState }) => 
-// 	(next) => (action) => {
-// 		console.log('Will dispatch: ', action);
-// 		console.log('current state: ', getState());
-// 		console.log('next state: ', getState(next(action)));
-// 		return next(action);
-// } 
-// const middleWare = applyMiddleware(actionLogger)
-
-// ================ LOGIN COMPONENTS ================ // 
-
-// ================ CHAT COMPONENTS ================= // 
-import ChatItem from './components/chat-components/chatItem'
-import ChatList from './containers/chatList'
-
-// ================ CHAT CONTAINERS ================= // 
-import App from './containers/chatContainer'
 	
 const store = createStore(chatApp);
 store.dispatch(actionNewChatItem('Initial Chat Item Yall'))
