0aad7a15e83f33be12c4e389ea27b0a51a29928c
Travis Ueki <travis.ueki@learningfuze.com>
2016-05-04 18:07:05 -0700

    working on middleware

--- src/containers/newChatItem.js
+++ src/containers/newChatItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import store from '../index'
+import store from '../reducers/chatReducer'
 import actionNewChatItem from '../actions/action-new-chat-text'
 
 let NewChatItem = ( store ) =>  {
--- src/reducers/chatReducer.js
+++ src/reducers/chatReducer.js
@@ -1,11 +1,17 @@
-import { combineReducers } from 'redux'
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import chatItemReducer from './reducer-chat-item'
 import chatListReducer from './reducer-chat-list'
+import authReducer from './authentication'
+import actionNewChatItem from '../actions/action-new-chat-text'
 
 const chatApp = combineReducers({ 	
 	// The keys of the reducers (chatItem, ) will be available on the state object
 	chatItem: chatItemReducer,
 	chatArray: chatListReducer
 });
+
+const store = createStore(chatApp, applyMiddleware(thunk));
+store.dispatch(actionNewChatItem('Initial Chat Item Yall'))
 
 export default chatApp
